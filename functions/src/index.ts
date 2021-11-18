09b127e39f91ddb734589d5149cf569ffdfdd9de
Maxim Stecenko <mxm.stecenko@gmail.com>
2021-11-18 07:54:02 +0200

    Fix storing products in functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,7 +10,7 @@ export const addProduct = functions.https.onRequest(async (request, response) =>
   const product = new Product(productName);
 
   try {
-    await admin.firestore().collection("products").add({...product});
+    await admin.firestore().collection("products").doc(product.id).set({...product});
     response.status(201).send(`Created product ${productName}`);
   } catch (e) {
     response.status(500).send(`Cannot insert new product with name ${productName}. ${e}`);
